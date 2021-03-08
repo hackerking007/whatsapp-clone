3f1b5df25c293d0d36da13c49326221fce5cd5cd
hackerking007 <ayjain71@gmail.com>
2021-03-08 20:46:17 +0530

    3rd commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,8 @@ import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import { Avatar, IconButton } from "@material-ui/core";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
+
 
 function Sidebar() {
     return (
@@ -22,6 +24,14 @@ function Sidebar() {
                     </IconButton>
                 </div>
             </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text"></input>
+                </div>
+            </div>
+
         </div>
     );
 }
