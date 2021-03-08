a63ea1a4fc1f7bbddca028503adb4498597e3abf
hackerking007 <ayjain71@gmail.com>
2021-03-08 20:20:06 +0530

    2nd commit

---
+++ src/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyArvDG4qFMdkeb_UHJYcTg5KVVKUGWtE4U",
+    authDomain: "whatsapp-mern-clone-44667.firebaseapp.com",
+    projectId: "whatsapp-mern-clone-44667",
+    storageBucket: "whatsapp-mern-clone-44667.appspot.com",
+    messagingSenderId: "1033639183978",
+    appId: "1:1033639183978:web:8013d637d5860f49f7ec13"
+  };
